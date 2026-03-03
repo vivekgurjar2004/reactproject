f0c0693f1b684ee05712bb8f01f1f5c2fbdbb469
vivekgurjar2004 <vivekgurjar919@gmail.com>
2026-03-03 10:12:58 +0530

    Fix import for PostContext in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useContext, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { PostProvider,postContext } from './PostContext'
+import { PostProvider,PostContext } from './PostContext'
 import PostList from './PostList'
 import Pagination from './Pagination'
 
